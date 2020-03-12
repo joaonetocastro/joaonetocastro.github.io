1f4ad383739b62340a48ee5b7d208f8a86f23cc0
Joao Neto <jneto111@gmail.com>
2020-03-11 21:20:22 -0300

    Updates

---
+++ precache-manifest.5b7782facebaecbb14506e0f0edf3d7d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb77f1513d11af5e30f2f862dff9ac67",
+    "url": "/joaonetocastro.github.io/index.html"
+  },
+  {
+    "revision": "8c966650a06ea46d032a",
+    "url": "/joaonetocastro.github.io/static/css/main.68a1bfbc.chunk.css"
+  },
+  {
+    "revision": "ae8cb14e1e67eb3a5ed5",
+    "url": "/joaonetocastro.github.io/static/js/2.7b6d3a8e.chunk.js"
+  },
+  {
+    "revision": "ac9fac6c8e755a8efecd01981adb88e6",
+    "url": "/joaonetocastro.github.io/static/js/2.7b6d3a8e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c966650a06ea46d032a",
+    "url": "/joaonetocastro.github.io/static/js/main.fec61fdf.chunk.js"
+  },
+  {
+    "revision": "a2e8068486f3795d2dda",
+    "url": "/joaonetocastro.github.io/static/js/runtime-main.ad96e239.js"
+  }
+]);
