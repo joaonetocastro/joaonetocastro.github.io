fdd24a0d168218396913ca4963ce2e395585c3d1
Joao Neto <jneto111@gmail.com>
2020-03-12 15:29:16 -0300

    padding

---
+++ precache-manifest.377dc134a79825800b31278cb3b7e6cf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c1255beefed87d33da8b5c06ba10bdc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3fa0219fcefc27f673ee",
+    "url": "/static/css/main.68a1bfbc.chunk.css"
+  },
+  {
+    "revision": "2b0c318d13111a4c3b9e",
+    "url": "/static/js/2.f152f404.chunk.js"
+  },
+  {
+    "revision": "ac9fac6c8e755a8efecd01981adb88e6",
+    "url": "/static/js/2.f152f404.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3fa0219fcefc27f673ee",
+    "url": "/static/js/main.6c6999b0.chunk.js"
+  },
+  {
+    "revision": "2b748575d325adc11ce4",
+    "url": "/static/js/runtime-main.493d5184.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/joaonetocastro.github.io/precache-manifest.5b7782facebaecbb14506e0f0edf3d7d.js"
+  "/precache-manifest.377dc134a79825800b31278cb3b7e6cf.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/joaonetocastro.github.io/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
