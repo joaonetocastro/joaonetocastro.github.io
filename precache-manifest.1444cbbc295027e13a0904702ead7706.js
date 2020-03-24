523770b1d34563516de5a85475603d9a45a0b11b
João Neto Castro <joaonetoc@pop-os.localdomain>
2020-03-24 13:48:30 -0300

    transfering build files to master

---
+++ precache-manifest.1444cbbc295027e13a0904702ead7706.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e597bb248b6b197e1b066d146a71e19",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e4e777e19870865dbc51",
+    "url": "/static/css/main.7cc69896.chunk.css"
+  },
+  {
+    "revision": "92ba242ded28908dc097",
+    "url": "/static/js/2.538ab82b.chunk.js"
+  },
+  {
+    "revision": "ac9fac6c8e755a8efecd01981adb88e6",
+    "url": "/static/js/2.538ab82b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e4e777e19870865dbc51",
+    "url": "/static/js/main.f94cb33c.chunk.js"
+  },
+  {
+    "revision": "2b748575d325adc11ce4",
+    "url": "/static/js/runtime-main.493d5184.js"
+  }
+]);
