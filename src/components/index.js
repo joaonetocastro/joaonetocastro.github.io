d6251958b4428085901bbdfe1139527739c8c704
João Neto Castro <joaonetoc@pop-os.localdomain>
2020-04-01 13:21:41 -0300

    creating components/index

---
+++ src/components/index.js
@@ -0,0 +1,8 @@
+const Header = require('./Header');
+const MySkills = require('./MySkills');
+const ProjectsGrid = require('./ProjectsGrid');
+const DownloadMyCV = require('./DownloadMyCV');
+const LetsWorkTogether = require('./LetsWorkTogether');
+const AboutMe = require('./AboutMe');
+
+module.export = {Header, MySkills, ProjectsGrid, DownloadMyCV, LetsWorkTogether, AboutMe};
