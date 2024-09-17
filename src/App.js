ab12ced134052be53bca86ca1a4a2f7981faf7a9
João Neto C <j.neto111@gmail.com>
2024-09-17 17:36:32 -0300

    feat: app

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import linkedinLogo from './imgs/linkedin.svg';
+import githubLogo from './imgs/github.svg';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <h1>João Neto Castro</h1>
+        <p className="description">
+          Sou desenvolvedor Full Stack com experiência em JavaScript, Node, TypeScript, Python, Django, FastAPI, MySQL, Postgres, Docker, CI, CD.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="links">
+          <a href="https://www.linkedin.com/in/joaonetoc/" target="_blank" rel="noopener noreferrer">
+            <img src={linkedinLogo} alt="LinkedIn" className="logo" />
+            <span>LinkedIn</span>
+          </a>
+          <a href="https://github.com/joaonetocastro" target="_blank" rel="noopener noreferrer">
+            <img src={githubLogo} alt="GitHub" className="logo" />
+            <span>GitHub</span>
+          </a>
+        </div>
       </header>
     </div>
   );
